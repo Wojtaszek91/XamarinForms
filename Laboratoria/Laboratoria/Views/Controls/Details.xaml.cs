bc9a35371911edad65a61ac972d6a77bef9cd685
Wojtaszek91 <spiochtar@o2.pl>
2020-06-08 21:08:39 +0200

    Add project files.

---
+++ Laboratoria/Laboratoria/Views/Controls/Details.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+
+namespace Laboratoria.Views.Controls
+{
+    public partial class Details : StackLayout
+    {
+        public Details()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+    propertyName: nameof(Title),
+    returnType: typeof(string),
+    declaringType: typeof(Details),
+    defaultValue: null);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty ControlContentProperty = BindableProperty.Create(
+    propertyName: nameof(ControlContent),
+    returnType: typeof(View),
+    declaringType: typeof(Details),
+    defaultValue: null);
+        public View ControlContent
+        {
+            get { return (View)GetValue(ControlContentProperty); }
+            set { SetValue(ControlContentProperty, value); }
+        }
+    }
+}
---
+++ Laboratoria/Laboratoria/Views/Controls/View1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratoria.Views.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View1 : ContentView
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+    propertyName: nameof(Title),
+    returnType: typeof(string),
+    declaringType: typeof(ContentWithHeader),
+    defaultValue: null);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty ControlContentProperty = BindableProperty.Create(
+    propertyName: nameof(ControlContent),
+    returnType: typeof(string),
+    declaringType: typeof(ContentWithHeader),
+    defaultValue: null);
+
+        public string ControlContent
+
+        {
+            get { return (string)GetValue(ControlContentProperty); }
+            set { SetValue(ControlContentProperty, value); }
+        }
+
+        public View1()
+        {
+            InitializeComponent();
+        }
+    }
+}
