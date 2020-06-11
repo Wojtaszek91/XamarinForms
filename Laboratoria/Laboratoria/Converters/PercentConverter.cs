cc7327feeede7ad88cdb7e0f2783df793a93a6bb
Wojtaszek91 <spiochtar@o2.pl>
2020-06-11 16:08:32 +0200

    add converter and binding to values in detailspage

---
+++ Laboratoria/Laboratoria/Converters/PercentConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Laboratoria.Converters
+{
+    class PercentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(double.TryParse(value.ToString(), out var convertedValue))
+            {
+                return convertedValue / 100;
+            }
+            return value;          
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
