7462cb31a1805113e4dcb4780a153913384deda0
Wojtaszek91 <spiochtar@o2.pl>
2020-06-12 13:09:31 +0200

    add getlocation method

--- Laboratoria/Laboratoria/ViewModels/HomeViewModel.cs
+++ Laboratoria/Laboratoria/ViewModels/HomeViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Laboratoria.ViewModels
@@ -17,11 +20,43 @@ namespace Laboratoria.ViewModels
         public HomeViewModel(INavigation navigation)
         {
             _navigation = navigation;
+            var location = GetLocation();
+            Initialize();
         }
 
         private void NavitageToDetailsPage()
         {
             _navigation.PushAsync(new DetailsPage());
+        }
+
+        private void Initialize()
+        {
+
+        }
+
+
+
+
+
+        private static HttpClient GetClientWithHeaders()
+        {
+            var client = new HttpClient();
+
+
+            return client;
+        }
+        private async Task<Location> GetLocation()
+        {
+            try
+            {
+                return await Geolocation.GetLastKnownLocationAsync() 
+                    ?? await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Low));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
         }
     }
 }
